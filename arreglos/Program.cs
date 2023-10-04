6d1da2ea4d38c483ce4872fcdbece1ab7b7dc435
FranciZco GuZmán <FranciZco GuZmn@DESKTOP-HH5SITF>
2023-10-03 21:59:29 -0700

    Agregar archivos de proyecto.

---
+++ arreglos/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace arreglos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            arreglitos arreglitos = new arreglitos();
+            //int[] arreglo = new int[10];
+
+            // Inicializar el arreglo
+            /*arreglitos.InicializarArreglo(arreglo);
+
+            // Mostrar el arreglo
+
+
+            // Calcular y mostrar la media
+            double media = arreglitos.CalcularMedia(arreglo);
+            Console.WriteLine("La media de los valores en el arreglo es: " + media);*/
+
+            /*int tamaño;
+            int numero;
+
+            Console.Write("Ingrese el tamaño del arreglo: ");
+            int.TryParse(Console.ReadLine(), out tamaño);
+
+            Console.Write("Ingrese el número para generar múltiplos: ");
+            int.TryParse(Console.ReadLine(), out numero);
+
+            if (tamaño <= 0 || numero <= 0)
+            {
+                Console.WriteLine("Entrada no válida. Tanto el tamaño como el número deben ser mayores que cero.");
+                return;
+            }
+
+            int[] arreglo = arreglitos.GenerarMultiplos(tamaño, numero);
+            arreglitos.MostrarArreglos(arreglo);
+            arreglitos.ProcesarArreglos(); */
+        }
+    }
+
+
---
+++ arreglos/arreglos/arreglitos.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arreglos.arreglos
+{
+    internal class arreglitos
+    {
+        public int[] InicializarArreglo()
+        {
+            int[] array = new int[5];
+            array[0] = 1;
+            array[1] = 2;
+            array[2] = 3;
+            array[3] = 4;
+            array[4] = 5;
+            return array;
+        }
+
+        public void MostrarArreglo(int[] array)
+        {
+            foreach (int valor in array)
+            {
+                Console.WriteLine(valor);
+            }
+        }
+
+        public void InicializarArreglo(int[] arreglo)
+        {
+            arreglo[0] = 1;
+            arreglo[1] = 2;
+            arreglo[2] = 3;
+            arreglo[3] = 4;
+            arreglo[4] = 5;
+            arreglo[5] = 6;
+            arreglo[6] = 7;
+            arreglo[7] = 8;
+            arreglo[8] = 9;
+            arreglo[9] = 10;
+        }
+
+        public double CalcularMedia(int[] arreglo)
+        {
+            double suma = 0;
+            foreach (int valor in arreglo)
+            {
+                suma += valor;
+            }
+            return suma / arreglo.Length;
+        }
+
+        public void MostrarMedia(double media)
+        {
+            Console.WriteLine("La media de los valores en el arreglo es: " + media);
+        }
+
+        public int[] GenerarMultiplos(int tamaño, int numero)
+        {
+            int[] arreglo = new int[tamaño];
+
+            for (int i = 0; i < tamaño; i++)
+            {
+                arreglo[i] = (i + 1) * numero;
+            }
+
+            return arreglo;
+        }
+
+
+        public void MostrarArreglos(int[] arreglo)
+        {
+            Console.WriteLine("Valores del arreglo:");
+            foreach (int valor in arreglo)
+            {
+                Console.WriteLine(valor);
+            }
+        }
+
+        //NO LE ENTENDI
+        /*static void ProcesarArreglos()
+        {
+            Console.Write("Ingrese el tamaño de los arreglos: ");
+            int tamaño = LeerEnteroPositivo();
+
+            if (tamaño < 1)
+            {
+                Console.WriteLine("Entrada no válida. El tamaño debe ser mayor que cero.");
+                return;
+            }
+
+            string[] nombres = new string[tamaño];
+            int[] longitudes = new int[tamaño];
+
+            for (int i = 0; i < tamaño; i++)
+            {
+                Console.Write($"Ingrese el nombre {i + 1}: ");
+                nombres[i] = Console.ReadLine();
+                longitudes[i] = nombres[i].Length;
+            }
+
+            MostrarNombresYLongitudes(nombres, longitudes);
+        }
+
+        static int LeerEnteroPositivo()
+        {
+            int numero;
+            int.TryParse(Console.ReadLine(), out numero);
+            return numero;
+        }
+
+        static void MostrarNombresYLongitudes(string[] nombres, int[] longitudes)
+        {
+            Console.WriteLine("Nombres y sus longitudes:");
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                Console.WriteLine($"{nombres[i]} - {longitudes[i]} caracteres");
+            }
+        }*/
+    }
+}
